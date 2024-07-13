6df34348389b6593dd74214f41dc8e96627f4e44
Frederik <frederikblockchain@gmail.com>
2024-07-13 17:10:24 +0200

    First version of UI up and running

--- core/src/com/mygdx/game/UI/PauseScreen/PauseScreen.kt
+++ core/src/com/mygdx/game/UI/PauseScreen/PauseScreen.kt
@@ -1,17 +1,26 @@
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Button
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import com.mygdx.game.GameModes.GameMode
 import com.mygdx.game.GameModes.changeMode
+import com.mygdx.game.GameObjects.Animelia.ANIMELIA_ENTITY
+import com.mygdx.game.GameObjects.Animelia.ANIMELIA_STAGE
+import com.mygdx.game.GameObjects.Animelia.getAnimeliaData
 
 lateinit var rootTable: Table
 
@@ -29,17 +38,45 @@ class PauseScreen(val prevMode: GameMode, val stage: Stage){
         rootTable.setFillParent(true)
 
         val juniorTable = Table()
+        val masterTable = Table()
+
+        val backgroundColor = Color(1f, 1f, 1f, 1f) // Example color
+
+        rootTable.background = createBackgroundDrawable(backgroundColor)
 
         stage.addActor(rootTable)
-        rootTable.add(juniorTable)
+        rootTable.add(juniorTable).padRight(100f)
+        rootTable.add(masterTable).padLeft(100f)
+        val skin = Skin(Gdx.files.internal("assets/ui/uiskin.json"))
+
+        val labelStyle = Label.LabelStyle(FontManager.ChapterFont, Color.BLACK)
+
+        val juniorLabel = Label("Junior", labelStyle)
+        val masterLabel = Label("Master", labelStyle)
+
+        juniorTable.add(juniorLabel).size(50f).padBottom(20f)
+        masterTable.add(masterLabel).size(50f).padBottom(20f)
+        juniorTable.row()
+        masterTable.row()
+
+        ANIMELIA_ENTITY.entries.forEach {
+            val data = getAnimeliaData(it)
+            val button = Button(skin)
+            if(data.animeliaStage == ANIMELIA_STAGE.JUNIOR){
+                juniorTable.add(button).width(100f).height(100f).padBottom(100f)
+                juniorTable.row()
+            }
+            else {
+                masterTable.add(button).width(100f).height(100f).padBottom(100f)
+                masterTable.row()
+            }
+            buttons.add(button)
+        }
+
+
 
 
         //table.debug = true // This is optional, but enables debug lines for tables.
-
-        val skin = Skin(Gdx.files.internal("assets/ui/uiskin.json"))
-        val button = Button(skin)
-        val button2 = Button(skin)
-        val button3 = Button(skin)
 
         stage.addListener(object : InputListener() {
             override fun keyDown(event: InputEvent?, keycode: Int): Boolean {
@@ -66,13 +103,13 @@ class PauseScreen(val prevMode: GameMode, val stage: Stage){
             }
         })
 
-        juniorTable.add(button).center().width(100f).height(100f)
+        /*juniorTable.add(button).center().width(100f).height(100f)
         juniorTable.row()
         juniorTable.add(button2).center().width(100f).height(100f)
         juniorTable.row()
         juniorTable.add(button3).center().width(100f).height(100f)
 
-        buttons.addAll(listOf(button, button2, button3))
+        buttons.addAll(listOf(button, button2, button3))*/
 
         setActiveButton(buttons[0])
 
@@ -87,7 +124,7 @@ class PauseScreen(val prevMode: GameMode, val stage: Stage){
     fun render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         stage.act(Gdx.graphics.deltaTime)
-        stage.isDebugAll = true
+      //  stage.isDebugAll = true
         stage.draw()
 
         activeButton?.let {
@@ -129,5 +166,24 @@ class PauseScreen(val prevMode: GameMode, val stage: Stage){
         button.fire(event)
         event.type = InputEvent.Type.touchUp
         button.fire(event)
+    }
+
+    fun getAllAnimelias(){
+    }
+
+    private fun createBackgroundDrawable(color: Color): Drawable {
+        // Create a pixmap with the specified color
+        val pixmap = Pixmap(1, 1, Pixmap.Format.RGBA8888)
+        pixmap.setColor(color)
+        pixmap.fill()
+
+        // Create a texture from the pixmap
+        val texture = Texture(pixmap)
+
+        // Dispose of the pixmap to free up memory
+        pixmap.dispose()
+
+        // Create a drawable from the texture
+        return TextureRegionDrawable(texture)
     }
 }
