29d92ae113f0ae6d50f57ec492a96300d056c0bd
Frederik <frederikblockchain@gmail.com>
2024-08-07 12:28:27 +0200

    Added item gained abilities

--- core/src/com/mygdx/game/Ability/AbilityItem.kt
+++ core/src/com/mygdx/game/Ability/AbilityItem.kt
@@ -1,10 +1,19 @@
 package com.mygdx.game.Ability
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.math.Vector2
+import com.mygdx.game.Animation.TextAnimation
+import com.mygdx.game.Collition.Collision
+import com.mygdx.game.Collition.CollisionMask
+import com.mygdx.game.Collition.MoveCollision
+import com.mygdx.game.Collition.OnlyPlayerCollitionMask
 import com.mygdx.game.DefaultTextureHandler
 import com.mygdx.game.Enums.Layer
 import com.mygdx.game.GameObjectData
 import com.mygdx.game.GameObjects.GameObject.GameObject
+import com.mygdx.game.Managers.AnimationManager
+import com.mygdx.game.plus
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromJsonElement
@@ -15,6 +24,8 @@ class AbilityItem(gameObjectData: GameObjectData) : GameObject(gameObjectData) {
     val abilityString = Json.decodeFromJsonElement<AbilityCustomFields>(gameObjectData.customFields).Ability
     val abilityType = convertAbilityToType(abilityString)
     val abilityData = convertAbilityTypeToData(abilityType)
+    override val collitionMask = OnlyPlayerCollitionMask
+    override val collision = AbilityItemCollision(this)
     override val texture = DefaultTextureHandler.getTexture(abilityData.imageIcon)
 
     init {
@@ -26,4 +37,32 @@ class AbilityItem(gameObjectData: GameObjectData) : GameObject(gameObjectData) {
 @Serializable
 data class AbilityCustomFields(val Ability: String){
 
+}
+
+class AbilityItemCollision(val abilityItem: AbilityItem): MoveCollision(){
+    override var canMoveAfterCollision = true
+
+    override fun collisionHappened(collidedObject: GameObject) {
+        abilityItem.remove()
+        val textAnimation = TextAnimation(
+            Color.GREEN,
+            "You Gained the ${abilityItem.abilityType.name} Ability",
+            abilityItem.currentMiddle,
+            false,
+            120
+        )
+        AnimationManager.animationManager.add(textAnimation)
+
+        if (abilityItem.abilityType == AbilityType.Fireball) {
+            val textAnimation2 = TextAnimation(
+                Color.WHITE,
+                "Press Escape to switch abilities",
+                abilityItem.currentMiddle + Vector2(-75f, 100f),
+                false,
+                420
+            )
+            AnimationManager.animationManager.add(textAnimation2)
+
+        }
+    }
 }
