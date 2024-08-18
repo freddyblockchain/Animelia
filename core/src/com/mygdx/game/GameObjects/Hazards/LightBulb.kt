289d1288fb074ad594bf07dc490afeb880ae2150
freddy212 <201406525@post.au.dk>
2024-08-18 17:05:39 +0200

    Add fireball icon to bell

--- core/src/com/mygdx/game/GameObjects/Hazards/LightBulb.kt
+++ core/src/com/mygdx/game/GameObjects/Hazards/LightBulb.kt
@@ -1,9 +1,9 @@
 package com.mygdx.game.GameObjects.Hazards
 
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
-import com.mygdx.game.Collisions.CanMoveCollision
 import com.mygdx.game.Collition.MoveCollision
-import com.mygdx.game.Collition.OnlyPlayerCollitionMask
 import com.mygdx.game.Collition.OnlyProjectileCollisionMask
 import com.mygdx.game.DefaultSoundHandler
 import com.mygdx.game.DefaultTextureHandler
@@ -12,7 +12,6 @@ import com.mygdx.game.Enums.Layer
 import com.mygdx.game.GameObjectData
 import com.mygdx.game.GameObjects.GameObject.GameObject
 import com.mygdx.game.GameObjects.MoveableObjects.Projectile.Fireball
-import com.mygdx.game.GameObjects.Triggers.AbilityCustomFields
 import com.mygdx.game.Managers.AreaManager
 import com.mygdx.game.Utils.Triggerable
 import kotlinx.serialization.Serializable
@@ -22,6 +21,7 @@ import kotlinx.serialization.json.decodeFromJsonElement
 class LightBulb(gameObjectData: GameObjectData)
     : GameObject(gameObjectData, Vector2(gameObjectData.width.toFloat(),gameObjectData.height.toFloat())){
     override val texture = DefaultTextureHandler.getTexture("lightbulb.png")
+    val fireIconSprite = Sprite(DefaultTextureHandler.getTexture("fireball-icon.png"))
     override val layer = Layer.PERSON
     override val collitionMask = OnlyProjectileCollisionMask
     override val collision = LightBulbCollision(this)
@@ -31,6 +31,17 @@ class LightBulb(gameObjectData: GameObjectData)
 
     override fun initObject() {
         gameObjectToTrigger = AreaManager.getObjectWithIid(entityRefData.entityIid, entityRefData.levelIid) as Triggerable
+
+        fireIconSprite.setPosition(currentMiddle.x - 8f,currentMiddle.y)
+        fireIconSprite.setSize(16f,16f)
+
+        val averageColor = (fireIconSprite.color.r + fireIconSprite.color.g + fireIconSprite.color.b) / 10.0f
+        fireIconSprite.setColor(averageColor, averageColor, averageColor, fireIconSprite.color.a)
+    }
+
+    override fun render(batch: SpriteBatch) {
+        super.render(batch)
+        fireIconSprite.draw(batch)
     }
 }
 
