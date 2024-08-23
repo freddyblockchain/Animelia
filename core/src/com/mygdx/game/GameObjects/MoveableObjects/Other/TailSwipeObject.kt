034fd6d841132cbcd4c106a68464ba84f82a04f5
Frederik <frederikblockchain@gmail.com>
2024-08-23 13:43:43 +0200

    Fix tailswipe

--- core/src/com/mygdx/game/GameObjects/MoveableObjects/Other/TailSwipeObject.kt
+++ core/src/com/mygdx/game/GameObjects/MoveableObjects/Other/TailSwipeObject.kt
@@ -2,40 +2,40 @@ package com.mygdx.game.GameObjects.MoveableObjects.Other
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
+import com.mygdx.game.*
+import com.mygdx.game.CannotMoveStrategy.CannotMoveStrategy
 import com.mygdx.game.CannotMoveStrategy.MoveRegardless
+import com.mygdx.game.CannotMoveStrategy.NoAction
 import com.mygdx.game.Collition.AllOtherObjectsCollisionMask
 import com.mygdx.game.Collition.MoveCollision
-import com.mygdx.game.DefaultTextureHandler
 import com.mygdx.game.Enums.Direction
 import com.mygdx.game.Enums.Layer
-import com.mygdx.game.GameObjectData
 import com.mygdx.game.GameObjects.GameObject.FightableObject
 import com.mygdx.game.GameObjects.GameObject.GameObject
 import com.mygdx.game.GameObjects.GameObject.MoveableObject
 import com.mygdx.game.GameObjects.Hazards.Rock
 import com.mygdx.game.Managers.AreaManager
-import com.mygdx.game.plus
-import com.mygdx.game.setSize
 
 class TailSwipeObject(gameObjectData: GameObjectData, size: Vector2, val objectAttached: FightableObject, val rotationAmount: Float) : MoveableObject(gameObjectData, size) {
 
-    override var speed = 0f
-    override val cannotMoveStrategy = MoveRegardless()
+    override var speed = 1f
+    override val cannotMoveStrategy = NoAction()
     override val texture = DefaultTextureHandler.getTexture("FireGate.png")
     override val layer = Layer.ONGROUND
     override var direction = Direction.DOWN
     override var canChangeDirection = true
     override val collision = TailSwipeCollision(this,objectAttached)
     override val collitionMask = AllOtherObjectsCollisionMask(objectAttached)
+    var objectAttachedPos = Vector2(0f,0f)
 
     val entitesHit = mutableMapOf<GameObject, Boolean>()
 
     init {
         setSize(Vector2(objectAttached.width / 5f, 6f))
         currentUnitVector = objectAttached.currentUnitVector
-        val amountToIncrease = Vector2(- (objectAttached.width / 2 * currentUnitVector.x), -(objectAttached.height / 2 * currentUnitVector.y))
+        val amountToIncrease = Vector2(- (objectAttached.width / 1.5f * currentUnitVector.x), -(objectAttached.height / 1.5f * currentUnitVector.y))
         this.polygon.setOrigin(- amountToIncrease.x, - amountToIncrease.y)
-        this.setPosition(objectAttached.currentMiddle + amountToIncrease)
+        this.setPosition(objectAttached.currentMiddle + amountToIncrease - Vector2((objectAttached.width / 5f) / 2, 6f / 2))
     }
 
     override fun render(batch: SpriteBatch) {
@@ -48,10 +48,17 @@ class TailSwipeObject(gameObjectData: GameObjectData, size: Vector2, val objectA
             this.setPosition(Vector2(objectAttached.currentPosition().x + objectAttached.width / 2, this.currentPosition().y))
         }*/
         currentUnitVector = objectAttached.currentUnitVector
-        this.setPosition(this.currentPosition() + currentUnitVector)
+        if(objectAttachedPos != objectAttached.currentPosition()){
+            //Corresponding object has moved
+            this.move(Vector2(currentUnitVector), speed)
+        } else{
+            //Dont move further
+            this.move(Vector2(currentUnitVector), 0f)
+        }
+        //this.setPosition(this.currentPosition() + currentUnitVector)
         this.rotateByAmount(rotationAmount)
         //cheating abit to trigger collisions
-        this.move(Vector2(0f, 0f), speed)
+        objectAttachedPos = objectAttached.currentPosition()
     }
 }
 
